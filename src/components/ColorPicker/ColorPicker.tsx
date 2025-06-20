fdd5f0e4f8fdbf7687387e4a325fd208c64b1e38
Alex <alexpleonhardt@gmail.com>
2025-06-20 16:27:37 -0400

    feat: reset color button

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import { HexColorInput, HexColorPicker } from "react-colorful";
+import resetIcon from "../../assets/refresh.svg";
 import './styles.css';
 
 interface PropTypes {
@@ -8,11 +10,20 @@ interface PropTypes {
 }
 
 export default function ColorPicker({ label, color, setColor }: PropTypes) {
+  const initialColor = useRef(color);
+
+  const resetColor = () => setColor(initialColor.current);
+
   return (
     <div className="color-picker">
       <label>{label}</label>
       <HexColorPicker color={color} onChange={setColor} />
-      <HexColorInput color={color} onChange={setColor} prefixed={true}/>
+      <div className="color-input-wrapper">
+        <HexColorInput color={color} onChange={setColor} prefixed={true}/>
+        <button onClick={resetColor}>
+          <img src={resetIcon} alt="Reset"/>
+        </button>
+      </div>
     </div>
   );
 }
